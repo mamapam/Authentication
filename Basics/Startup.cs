6e6dae402ee216988715fce445fd5657f3ebb973
mamapam <mamapam.dev@gmail.com>
2020-12-20 16:01:46 -0500

    Basic Auth of claims and identity and principal

--- Basics/Startup.cs
+++ Basics/Startup.cs
@@ -1,12 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Basics
 {
@@ -16,6 +11,14 @@ namespace Basics
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddAuthentication("CookieAuth")
+				.AddCookie("CookieAuth", config =>
+				{
+					config.Cookie.Name = "Grandmas.Cookie";
+					config.LoginPath = "/Home/Authenticate";
+				});
+
+			services.AddControllersWithViews();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +29,21 @@ namespace Basics
 				app.UseDeveloperExceptionPage();
 			}
 
+			// Looks at route being accessed and decides which endpoint to use
 			app.UseRouting();
 
+			// Who are you?
+			app.UseAuthentication();
+
+			// This must be after routing so it can actually authorize against route
+			// Are you allowed?
+			app.UseAuthorization();
+
+
+			// Maps to the endpoint that routing invokes
 			app.UseEndpoints(endpoints =>
 			{
-				endpoints.MapGet("/", async context =>
-				{
-					await context.Response.WriteAsync("Hello World!");
-				});
+				endpoints.MapDefaultControllerRoute();
 			});
 		}
 	}
