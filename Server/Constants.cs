0187fabb2e82d048b169bd65d5560db8b68103d6
mamapam <mamapam.dev@gmail.com>
2020-12-21 00:40:26 -0500

    Setup basic JWT implementation

---
+++ Server/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace Server
+{
+	public static class Constants
+	{
+		public const string Issuer = Audience;
+		public const string Audience = "https://localhost:44331/";
+		public const string Secret = "not_too_short_secret_otherwise_it_might_error";
+
+	}
+}
